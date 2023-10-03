872991c7b4e16b5f8d1d0435cbca65c4cfe0612f
aleksandrakuszewska <olakuszewska1@gmail.com>
2023-10-03 17:35:02 +0200

    Squash user

---
+++ src/main/java/org/example/User.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class User {
+    public void printUser(String name){
+        System.out.println(name);
+    }
+    public void setUser(String name){
+        System.out.println("Zapisuje usera" + name);
+    }
+}
