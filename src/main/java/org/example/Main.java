3c67399d689910bbc83295e186271fdbf6ff6909
aleksandrakuszewska <olakuszewska1@gmail.com>
2023-10-03 16:39:41 +0200

    git commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -9,11 +9,18 @@ public class Main {
         System.out.printf("Hello and welcome!");
 
         // Press ⌃R or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 6; i++) {
+        for (int i = 1; i <= 5; i++) {
 
             // Press ⌃D to start debugging your code. We have set one breakpoint
             // for you, but you can always add more by pressing ⌘F8.
             System.out.println("i = " + i);
         }
+
+    }
+    public void cos() {
+        System.out.println("cos wystepuje 1");
+    }
+    public void cos2() {
+        System.out.println("cos wystepuje 2");
     }
 }
